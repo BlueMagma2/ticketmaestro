840cac160ff3a58f582819f97310b6a55105fc89
Jimmy Huguet <jimmyhuguet@gmail.com>
2015-07-23 17:23:54 +0200

    load event in bookingevent constructor + test if you get the right event or an error

--- booking/booking.py
+++ booking/booking.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from booking.utils.singleton import Singleton
 from booking.models import Venue, Event, Section, Row, Seat
 
@@ -9,13 +10,19 @@ class Booking:
         self.events = {}
 
     def get_event(self, event_pk):
+        """ return the Booking event object we are looking for or create it and add it to the events dict """
         if event_pk in self.events:
             return self.events[event_pk]
         else:
-            event = BookingEvent(event_pk)
+            event = _BookingEvent(event_pk)
             self.events[event_pk] = event
             return event
 
-    class BookingEvent:
-        def __init__(self, event_pk):
-            self.event_pk = event_pk
+
+class _BookingEvent:
+    """ represent an event and contain all method to book ticket for this event """
+    def __init__(self, event_pk):
+        try:
+            self.event = Event.objects.get(pk=event_pk)
+        except:
+            raise Exception("No Event for this primary key in the database")
--- booking/tests.py
+++ booking/tests.py
@@ -1,12 +1,30 @@
 from django.test import TestCase
 from booking.booking import Booking
+from booking.models import Venue, Event, Section, Row, Seat
 
 
 class TestBooking(TestCase):
     """ test that booking is working like expected """
 
+    def setUp(self):
+        """ create tests data """
+        self.venue = Venue.objects.create(name="a venue")
+        self.event1 = Event.objects.create(venue=self.venue, name="first event")
+        self.event2 = Event.objects.create(venue=self.venue, name="second event")
+
     def test_singleton(self):
         """ make sure booking is a singleton """
         booking1 = Booking.Instance()
         booking2 = Booking.Instance()
-        self.assertEqual(booking1 is booking2, True)
+        self.assertTrue(booking1 is booking2)
+
+    def test_get_event(self):
+        """ make sure you get the event you want and that asking for a wrong primary key raise an error """
+        booking_event = Booking.Instance().get_event(self.event1.pk)
+        self.assertEqual(booking_event.event, self.event1)
+        error = False
+        try:
+            Booking.Instance().get_event(-1)
+        except:
+            error = True
+        self.assertTrue(error)
