7b7559a37d29802029df7bea7b6ef83f9e8d2a80
Jimmy Huguet <jimmyhuguet@gmail.com>
2015-07-23 16:18:41 +0200

    add class for booking for a specific event and add function to access the object through Booking

--- booking/booking.py
+++ booking/booking.py
@@ -4,5 +4,18 @@ from booking.models import Venue, Event, Section, Row, Seat
 
 @Singleton
 class Booking:
+    """ serve the BookingEvent corresponding to the event we want to book for """
     def __init__(self):
-        print('booking created')
+        self.events = {}
+
+    def get_event(self, event_pk):
+        if event_pk in self.events:
+            return self.events[event_pk]
+        else:
+            event = BookingEvent(event_pk)
+            self.events[event_pk] = event
+            return event
+
+    class BookingEvent:
+        def __init__(self, event_pk):
+            self.event_pk = event_pk
