92410a228a582be46687fa00cb41490f88a4d0a8
Jimmy Huguet <jimmyhuguet@gmail.com>
2015-07-24 10:24:13 +0200

    construct booking data tree + test if correctly constructed

--- booking/booking.py
+++ booking/booking.py
@@ -26,3 +26,36 @@ class _BookingEvent:
             self.event = Event.objects.get(pk=event_pk)
         except:
             raise Exception("No Event for this primary key in the database")
+        self.venue = self.event.venue
+
+        # construct tree of sections/rows/seats to access information faster
+        self.sections = []
+
+        for section in self.venue.section_set.all().order_by("rank"):
+            section_data = {"name":section.label, "rank":section.rank, "max_adjacent_seat":0, "seats_available": 0, "rows": []}
+            self.sections.append(section_data)
+
+            for row in section.row_set.all().order_by("rank"):
+                row_data = {"name":row.label, "rank":row.rank, "max_adjacent_seat":0, "seats_available": 0}
+                section_data["rows"].append(row_data)
+
+                # we count how many seats and adjacent seats are available
+                seats_available = row.seat_set.all().filter(booked=False).order_by("number")
+                row_data["seats_available"] = seats_available.count()
+                section_data["seats_available"] += row_data["seats_available"]
+
+                counter = 0 # count adjacent seat
+                last_number = -1 # seat number of the previous iteration
+                for seat in seats_available:
+                    if seat.number > last_number + 1:
+                        if counter > row_data["max_adjacent_seat"]:
+                            row_data["max_adjacent_seat"] = counter
+                        counter = 0
+                    last_number = seat.number
+                    counter += 1
+
+                if counter > row_data["max_adjacent_seat"]:
+                    row_data["max_adjacent_seat"] = counter
+
+                if row_data["max_adjacent_seat"] > section_data["max_adjacent_seat"]:
+                    section_data["max_adjacent_seat"] = row_data["max_adjacent_seat"]
--- booking/tests.py
+++ booking/tests.py
@@ -12,6 +12,31 @@ class TestBooking(TestCase):
         self.event1 = Event.objects.create(venue=self.venue, name="first event")
         self.event2 = Event.objects.create(venue=self.venue, name="second event")
 
+        self.sections = [];
+        self.sections.append(Section.objects.create(venue=self.venue, label="east", rank=3))
+        self.sections.append(Section.objects.create(venue=self.venue, label="north", rank=3))
+        self.sections.append(Section.objects.create(venue=self.venue, label="VIP", rank=1))
+        self.sections.append(Section.objects.create(venue=self.venue, label="south", rank=3))
+        self.sections.append(Section.objects.create(venue=self.venue, label="first class", rank=2))
+        self.sections.append(Section.objects.create(venue=self.venue, label="west", rank=3))
+
+        self.rows = {}
+        for section_index in range(len(self.sections)):
+            section = self.sections[section_index]
+            self.rows[section.label] = []
+            for i in range(5):
+                row = Row.objects.create(section=section, label="east", rank=i)
+                self.rows[section.label].append(row)
+                for s in range(5):
+                    if section_index == 2:
+                        Seat.objects.create(row=row, number=s, booked=True, booked_for="the queen")
+                    elif section_index == 4 and i == 0:
+                        Seat.objects.create(row=row, number=s, booked=True, booked_for="some first class fan")
+                    elif section_index == 0 and s == 2:
+                        Seat.objects.create(row=row, number=s, booked=True, booked_for="middle watcher")
+                    else:
+                        Seat.objects.create(row=row, number=s, booked=False, booked_for="")
+
     def test_singleton(self):
         """ make sure booking is a singleton """
         booking1 = Booking.Instance()
@@ -28,3 +53,23 @@ class TestBooking(TestCase):
         except:
             error = True
         self.assertTrue(error)
+
+    def test_booking_tree(self):
+        """ make sure the booking tree is correctly constructed with correct data """
+        tree = Booking.Instance().get_event(self.event1.pk).sections
+        self.assertEqual(len(tree), 6)
+        self.assertEqual(tree[0]["name"], "VIP")
+        self.assertEqual(tree[0]["seats_available"], 0)
+        self.assertEqual(tree[0]["max_adjacent_seat"], 0)
+
+        self.assertEqual(tree[1]["name"], "first class")
+        self.assertEqual(tree[1]["seats_available"], 20)
+        self.assertEqual(tree[1]["max_adjacent_seat"], 5)
+        self.assertEqual(tree[1]["rows"][0]["max_adjacent_seat"], 0)
+
+        self.assertEqual(tree[2]["name"], "east")
+        self.assertEqual(tree[2]["seats_available"], 20)
+        self.assertEqual(tree[2]["max_adjacent_seat"], 2)
+
+        self.assertEqual(tree[3]["seats_available"], 25)
+        self.assertEqual(tree[3]["max_adjacent_seat"], 5)
